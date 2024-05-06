ea6a1247bde0991f0da2979218a262e6026c6e19
tik <thiagoiklock@gmail.com>
2024-05-05 23:23:15 -0300

    git init

---
+++ home/views.py
@@ -0,0 +1,16 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+
+def homepage(request):
+    time_now = datetime.datetime.now()
+    html_to_display = f"""
+        <html>
+            <body>
+                <h2>It is now {time_now}</h2>
+            </body>
+        </html>
+    """
+    return HttpResponse(html_to_display)
